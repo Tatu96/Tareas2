bbf842f4b76a13e4ef761235e2448c22cadeda68
Sotom98 <sotom98@users.noreply.github.com>
2019-03-05 22:34:47 -0600

    Cmbs1

---
+++ AlturaBase/AlturaBase/Principal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Area;
+
+namespace AlturaBase {
+    class Principal {
+        public static void Main(string[] args) {
+            Metodos mts = new Metodos();
+
+            mts.Lineas();
+            Console.Write("Ingrese altura: ");
+            double a = double.Parse(Console.ReadLine());
+            mts.setAltrua(a);
+            mts.Lineas();
+
+            Console.Write("Ingrese base: ");
+            double b = double.Parse(Console.ReadLine());
+            mts.setBase(b);
+            mts.Lineas();
+
+            Console.WriteLine("Area: " + mts.getArea());
+            Console.WriteLine("Perimetro: " + mts.getPerim());
+            mts.Lineas();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ AlturaBase/Area/Metodos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area {
+    public class Metodos {
+        private double Altura, Base, Perim, Area;
+
+        public void setAltrua(double a) {
+            Altura = a;
+        }
+
+        public void setBase(double b) {
+            Base = b;
+        }
+
+        public double getArea() {
+            Area = Base * Altura;
+            return Area;
+        }
+
+        public double getPerim() {
+            Perim = (2 * Altura) + (2 * Base);
+            return Perim;
+        }
+
+        public void Lineas() {
+            Console.WriteLine("-------------------------");
+        }
+    }
+}
