3e9a926b62e20b35ba74b194fca4340c3d3b124d
Sotom98 <sotom98@users.noreply.github.com>
2019-03-07 23:03:46 -0600

    20190307

---
+++ BuenasNoches/Noches/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Metodos;
+
+namespace Noches {
+    class Program {
+        static void Main(string[] args) {
+            Pedir pd = new Pedir();
+
+            pd.Ejec();
+
+            Console.ReadKey();
+        }
+    }
+}
