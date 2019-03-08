9160f7a748a163a6dbccf399f92171c7a7f4aef2
Sotom98 <sotom98@users.noreply.github.com>
2019-03-07 22:21:17 -0600

    20190307

---
+++ Semaforo/Semaforo/Metodos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principal {
+    public class Metodos {
+        public void Green() {
+            Console.WriteLine("Adelante.");
+        }
+
+        public void Yellow() {
+            Console.WriteLine("Advertencia.");
+        }
+
+        public void Red() {
+            Console.WriteLine("Detenerse.");
+        }
+
+        public void Lineas() {
+            Console.WriteLine("--------------------");
+        }
+    }
+}
---
+++ Semaforo/Semaforo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principal {
+    class Program {
+        static void Main(string[] args) {
+            Metodos mts = new Metodos();
+
+            Console.WriteLine("¿El semaforo esta en?\n1. Verde\n2. Amarillo\n3. Rojo");
+            mts.Lineas();
+            int a = int.Parse(Console.ReadLine());
+            mts.Lineas();
+
+            switch (a) {
+                case 1:
+                    mts.Green();
+                    break;
+                case 2:
+                    mts.Yellow();
+                    break;
+                case 3:
+                    mts.Red();
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
